b515cefc9615e4196f371dad122aa5791f829c33
shivamprajapatii <shivamprajapati4794@gmail.com>
2024-12-30 06:55:46 +0530

    complete with all models

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -2,13 +2,42 @@ const { Type } = require("@aws-sdk/client-s3");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    username : { Type : String, required : true},
-    email : { type: String, required : true, unique: true},
-    password : { type : String },
-    CreateAT: { type: Date, default: Date.now }
+const UserSchema = new Schema({
+    username: {
+        Type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+    },
+    Repository: [
+        {
+            default: [],
+            type: Schema.Types.ObjectId,
+            ref: "Repository"
+        }
+    ],
+    FollowedUsers : [
+        {
+            default : [],
+            type : Schema.Types.ObjectId,
+            ref : "User"
+        }
+    ],
+    starRepository : [
+        {
+            default : [],
+            type : Schema.Types.ObjectId,
+            ref : "Repository"
+        }
+    ] 
 });
 
-const User = mongoose.Model("user", userSchema);
-
+const User = mongoose.Model("User", UserSchema);
 module.exports = { User };
